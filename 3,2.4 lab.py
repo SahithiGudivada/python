7ee12aa76b11990f1783845ff873b94cd5f665d3
SahithiGudivada <161479769+SahithiGudivada@users.noreply.github.com>
2024-03-17 13:39:14 +0530

    3.2.4 completed

---
+++ 3,2.4 lab.py
@@ -0,0 +1,11 @@
+
+secret_number = 777
+
+number = int(input("guess the secret number:"))
+while number != secret_number:
+         print("Ha ha! You're stuck in my loop!")
+         number=int(input())
+else: 
+        print("Well done, muggle! You are free now.")
+       
+
